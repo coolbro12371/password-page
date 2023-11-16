5368fddc45d8c0fca04e01f1d01ed2a9d6b87f77
coolbro12371 <82981948+coolbro12371@users.noreply.github.com>
2023-11-16 14:02:20 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+     function checkPswd() {
+      var confirmPassword = "admin8989123";
+      var password = document.getElementById("pswd").value;
+      if (password == confirmPassword) {
+        window.location = "https://fantastic-goldfish-7x6j5qw4jrwfww5p-3000.app.github.dev/";
+      }
+    }
